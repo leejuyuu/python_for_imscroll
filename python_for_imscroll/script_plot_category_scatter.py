f6c570a4a5cfc7b8fc61610b49035a6adb126f41
Tzu-Yu Lee <leejuyuu@gmail.com>
2020-03-19 01:26:21 +0800

    Change plotting style
    Change the category scatter plot to show the mean and standard deviation, with
    small markers showing the actual data points.
    
    Increase the font size.

--- python_for_imscroll/script_plot_category_scatter.py
+++ python_for_imscroll/script_plot_category_scatter.py
@@ -5,19 +5,23 @@ import seaborn as sns
 
 
 def main():
-    filepath = Path('/run/media/tzu-yu/linuxData/Research/PriA_project/analysis_result/20200312/20200312_compiled_dwell_for_graph.ods')
+    filepath = Path('/run/media/tzu-yu/linuxData/Research/PriA_project/analysis_result/20200317/20200317_compiled_dwell_for_graph.ods')
     dfs = pd.read_excel(filepath, engine='odf')
-    ax = sns.stripplot(x='nucleotide', y='tobs', jitter=False, data=dfs)
-    ax.set_ylabel(r'$\tau_{obs}$ (s)', fontsize=14)
-    ax.set_xlabel('Nucleotide', fontsize=14)
+    ax = sns.stripplot(x='nucleotide', y='tobs', jitter=True, data=dfs, color='gray', marker='X')
+    sns.pointplot(x='nucleotide', y='tobs', data=dfs, ci='sd', join=False, capsize=0.05)
+    ax.set_ylabel(r'$\tau_{obs}$ (s)', fontsize=18)
+    ax.set_xlabel('Nucleotide', fontsize=18)
+    ax.tick_params(labelsize=14)
     fig = ax.get_figure()
     fig.savefig(filepath.parent / 'temp_nucleotide_obs.svg', format='svg', Transparent=True,
                 dpi=300, bbox_inches='tight')
     fig.clf()
 
-    ax2 = sns.stripplot(x='nucleotide', y='toff', jitter=False, data=dfs)
-    ax2.set_ylabel(r'$\tau_{off}$ (s)', fontsize=14)
-    ax2.set_xlabel('Nucleotide', fontsize=14)
+    ax2 = sns.stripplot(x='nucleotide', y='toff', jitter=True, data=dfs, color='gray', marker='X')
+    sns.pointplot(x='nucleotide', y='toff', data=dfs, ci='sd', join=False, capsize=0.05)
+    ax2.set_ylabel(r'$\tau_{off}$ (s)', fontsize=18)
+    ax2.set_xlabel('Nucleotide', fontsize=18)
+    ax2.tick_params(labelsize=14)
     fig2 = ax2.get_figure()
     fig2.savefig(filepath.parent / 'temp_nucleotide_off.svg', format='svg', Transparent=True,
                 dpi=300, bbox_inches='tight')
