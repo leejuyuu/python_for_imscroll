503fdab1ba47fabc1f81557cc3d82a5148a9471a
Tzu-Yu Lee <leejuyuu@gmail.com>
2020-03-25 17:33:46 +0800

    Change plotting style to bar and jittered scatter

--- python_for_imscroll/script_plot_category_scatter.py
+++ python_for_imscroll/script_plot_category_scatter.py
@@ -5,25 +5,41 @@ import seaborn as sns
 
 
 def main():
-    filepath = Path('/run/media/tzu-yu/linuxData/Research/PriA_project/analysis_result/20200317/20200317_compiled_dwell_for_graph.ods')
+    def change_width(ax, new_value) :
+        for patch in ax.patches :
+            current_width = patch.get_width()
+            diff = current_width - new_value
+
+            # we change the bar width
+            patch.set_width(new_value)
+
+            # we recenter the bar
+            patch.set_x(patch.get_x() + diff * .5)
+
+    filepath = Path('/home/tzu-yu/Analysis_Results/20200317/20200317_compiled_dwell_for_graph.ods')
     dfs = pd.read_excel(filepath, engine='odf')
-    ax = sns.stripplot(x='nucleotide', y='tobs', jitter=True, data=dfs, color='gray', marker='X')
-    sns.pointplot(x='nucleotide', y='tobs', data=dfs, ci='sd', join=False, capsize=0.05)
+    fig, ax = plt.subplots(figsize=(4.8, 4.8))
+    sns.barplot(x='nucleotide', y='tobs', data=dfs, ci='sd', capsize=0.08,edgecolor='black', fill=False, linewidth=1.5, errwidth=2)
+    change_width(ax, .5)
+    sns.stripplot(x='nucleotide', y='tobs', jitter=True, data=dfs,  marker='o', color='w', edgecolors='black', linewidth=1, s=6)
     ax.set_ylabel(r'$\tau_{obs}$ (s)', fontsize=18)
     ax.set_xlabel('Nucleotide', fontsize=18)
+    ax.set_ylim(bottom=0)
     ax.tick_params(labelsize=14)
-    fig = ax.get_figure()
+    sns.despine(fig, ax)
     fig.savefig(filepath.parent / 'temp_nucleotide_obs.svg', format='svg', Transparent=True,
                 dpi=300, bbox_inches='tight')
     fig.clf()
 
-    ax2 = sns.stripplot(x='nucleotide', y='toff', jitter=True, data=dfs, color='gray', marker='X')
-    sns.pointplot(x='nucleotide', y='toff', data=dfs, ci='sd', join=False, capsize=0.05)
-    ax2.set_ylabel(r'$\tau_{off}$ (s)', fontsize=18)
-    ax2.set_xlabel('Nucleotide', fontsize=18)
-    ax2.tick_params(labelsize=14)
-    fig2 = ax2.get_figure()
-    fig2.savefig(filepath.parent / 'temp_nucleotide_off.svg', format='svg', Transparent=True,
+    fig, ax = plt.subplots(figsize=(4.8, 4.8))
+    sns.barplot(x='nucleotide', y='toff', data=dfs, ci='sd', capsize=0.08,edgecolor='black', fill=False, linewidth=1.5, errwidth=2)
+    change_width(ax, .5)
+    sns.stripplot(x='nucleotide', y='toff', jitter=True, data=dfs,  marker='o', color='w', edgecolors='black', linewidth=1, s=6)
+    ax.set_ylabel(r'$\tau_{off}$ (s)', fontsize=18)
+    ax.set_xlabel('Nucleotide', fontsize=18)
+    ax.tick_params(labelsize=14)
+    sns.despine(fig, ax)
+    fig.savefig(filepath.parent / 'temp_nucleotide_off.svg', format='svg', Transparent=True,
                 dpi=300, bbox_inches='tight')
 
 if __name__ == '__main__':
