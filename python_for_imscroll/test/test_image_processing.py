09dcb60a5ef2fa8ee7a771ad93220ea7654f05cc
Tzu-Yu Lee <leejuyuu@gmail.com>
2020-05-03 19:24:08 +0800

    Add init test for ImageSequence class
    Add a fake image as test data. The faked image is generated as 400x600
    array, with 20 frames. Each frame is consecutive integer from the frame
    number (starting from 1) to frame number + 400x600 -1, reshaped row by
    row.
    
    Add a test to specify a class called ImageSequence. This class will take
    in the fake image sequence and should have some attributes (width,
    height, length) accordingly.

---
+++ python_for_imscroll/test/test_image_processing.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+
+Add a fake image as test data. The faked image is generated as 400x600
+array, with 20 frames. Each frame is consecutive integer from the frame
+number (starting from 1) to frame number + 400x600 -1, reshaped row by
+row.
+
+"""
+import pathlib
+from python_for_imscroll import image_processing as imp
+
+def test_read_glimpse_image():
+    pass
+
+
+def test_image_sequence_class():
+    image_path = pathlib.Path(__file__).parent() / 'fake_im/'
+    image_sequence = imp.ImageSequence(image_path)
+    assert image_sequence.width == 600
+    assert image_sequence.height == 400
+    assert image_sequence.length == 20
