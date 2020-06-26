9c13108ea831650cff4b29cc9c1e52a3a04e184a
Tzu-Yu Lee <leejuyuu@gmail.com>
2020-06-26 23:00:10 +0800

    Add image view features
    - Limit the panning and zooming area
    - Allow cration of AOI markers

--- gui/image_view.py
+++ gui/image_view.py
@@ -9,7 +9,6 @@ def main():
     path = Path('/run/media/tzu-yu/linuxData/Research/PriA_project/20200206_mapping/map/hwligroup00688/')
     image_sequence = imp.ImageSequence(path)
     image = image_sequence.get_whole_stack()
-    print(image.shape)
 
 
     ## Always start by initializing Qt (only once per application)
@@ -17,10 +16,17 @@ def main():
     if app is None:
         app = QtWidgets.QApplication([])
 
-    imv = pg.ImageView()
+    vb = pg.ViewBox()
+    vb.setLimits(xMin=0, xMax=image_sequence.width, yMin=0, yMax=image_sequence.height)
+    imv = pg.ImageView(view=vb)
+    marker = pg.ScatterPlotItem()
+    marker.setBrush(255, 0, 0, 255)
+    marker.setData([150, 200], [220, 100], symbol='s', pen=(0, 0, 255), brush=None, size=5, pxMode=False)
+    vb.addItem(marker)
     # imv = QtWidgets.QWidget()
     imv.show()
     imv.setImage(image, axes={'t': 2, 'x': 1, 'y': 0})
+
 
 
     ## Start the Qt event loop
