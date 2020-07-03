4648da82f8fbe8d6affe4b27ce3e005a9e493717
Tzu-Yu Lee <leejuyuu@gmail.com>
2020-07-03 17:51:28 +0800

    Add fitting script for multiple observations
    For replicate of kobs and concentration settings. Add sigma computation
    to represent y error bar.

--- python_for_imscroll/visualization.py
+++ python_for_imscroll/visualization.py
@@ -143,6 +143,38 @@ def plot_scatter_and_linear_fit(x, y, fit_result: dict,
     fig.savefig(save_fig_path, format='svg', Transparent=True, dpi=300, bbox_inches='tight')
 
 
+def plot_error_and_linear_fit(x, y, y_err, fit_result: dict,
+                                save_fig_path: Path = None,
+                                x_label: str = '',
+                                y_label: str = '',
+                                left_bottom_as_origin=False,
+                                y_top=None,
+                                x_right=None):
+    fig, ax = plt.subplots(figsize=(4, 3))
+    ax.errorbar(x, y, yerr=y_err, marker='o', linestyle='', capsize=1)
+    line_x = np.array([min(x), max(x)])
+    line_y = line_x * fit_result['slope'] + fit_result['intercept']
+    ax.plot(line_x, line_y)
+    if left_bottom_as_origin:
+        ax.set_xlim(left=0)
+        ax.set_ylim(bottom=0)
+    if y_top is not None:
+        ax.set_ylim(top=y_top)
+    if x_right is not None:
+        ax.set_xlim(right=x_right)
+    if not x_label:
+        x_label = input('Enter x axis label:\n')
+    if not y_label:
+        y_label = input('Enter y axis label:\n')
+    ax.set_xlabel(x_label, fontsize=16)
+    ax.set_ylabel(y_label, fontsize=16)
+    ax.text(0.05, 0.9, r'$y = {:.7f}x + {:.5f}$'.format(fit_result['slope'],
+                                                       fit_result['intercept']),
+            transform=ax.transAxes, fontsize=8)
+    ax.text(0.05, 0.8, r'$R^2 = {:.5f}$'.format(fit_result['r_squared']),
+            transform=ax.transAxes, fontsize=8)
+    # plt.rcParams['svg.fonttype'] = 'none'
+    fig.savefig(save_fig_path, format='svg', Transparent=True, dpi=300, bbox_inches='tight')
 
 
 if __name__ == '__main__':
