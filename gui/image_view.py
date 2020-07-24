e5dc20997b4a223ad5eabe9fbb71a6fb43ffd3e8
Tzu-Yu Lee <leejuyuu@gmail.com>
2020-07-24 23:21:13 +0800

    Add controls to set spot picking parameters
    Create a list model and corresponding ListView item in the qml file. Use
    the SpinBox control to represent the paramter value.
    
    Issue: currently only supports integer value, still need some work to
    make it accept floating points.

--- gui/image_view.py
+++ gui/image_view.py
@@ -1,12 +1,25 @@
+from collections import namedtuple
 from pathlib import Path
 import sys
+import typing
 import numpy as np
 import pyqtgraph as pg
-from PySide2 import QtWidgets, QtQuickWidgets, QtCore
+from PySide2 import QtWidgets, QtQuickWidgets, QtCore, QtQml
+from PySide2.QtCore import Qt
 import python_for_imscroll.image_processing as imp
 
 pg.setConfigOption('background', 'w')
 pg.setConfigOption('foreground', 'k')
+
+SPOT_DIA_STR = 'Spot diameter'
+NOISE_DIA_STR = 'Noise diameter'
+SPOT_BRIGHTNESS_STR = 'Spot brightness'
+ValueRange = namedtuple('ValueRange', ('min', 'max', 'step'))
+SPOT_PARAMS_RANGE = {SPOT_DIA_STR: ValueRange(0, 100, 0.5),
+                     NOISE_DIA_STR: ValueRange(0, 10, 0.2),
+                     SPOT_BRIGHTNESS_STR: ValueRange(0, 1000, 1)}
+
+PROPERTY_NAME_ROLE = Qt.UserRole + 1
 
 class MyImageView(pg.ImageView):
 
@@ -39,6 +52,7 @@ class Model(QtCore.QObject):
         self.aois: imp.Aois = None
         self.image_sequence: imp.ImageSequence = None
         self._current_frame = 0
+        self.pick_spots_param = PickSpotsParam()
 
 
     def get_coords(self):
@@ -51,10 +65,11 @@ class Model(QtCore.QObject):
         self.aois = aois
 
     def pick_spots(self):
+        params = self.pick_spots_param.params
         self.aois = imp.pick_spots(self.image_sequence.get_averaged_image(start=self._current_frame, size=1),
-                                   threshold=100,
-                                   noise_dia=1,
-                                   spot_dia=5)
+                                   threshold=params[SPOT_BRIGHTNESS_STR],
+                                   noise_dia=params[NOISE_DIA_STR],
+                                   spot_dia=params[SPOT_DIA_STR])
         self.aois_changed.emit()
 
     @QtCore.Slot(int)
@@ -73,6 +88,71 @@ class Model(QtCore.QObject):
         elif new_value < 0:
             new_value = 0
         self._current_frame = new_value
+
+    def _read_pick_spots_param(self):
+        return self.pick_spots_param
+
+    @QtCore.Signal
+    def dummy_notify(self):
+        pass
+
+    pickSpotsParam = QtCore.Property(QtCore.QObject,
+                                     fget=_read_pick_spots_param,
+                                     notify=dummy_notify)
+
+class PickSpotsParam(QtCore.QAbstractListModel):
+    def __init__(self):
+        super().__init__()
+        NV = namedtuple('NV', ['name', 'value'])
+        self.property_names = [SPOT_DIA_STR, NOISE_DIA_STR, SPOT_BRIGHTNESS_STR]
+        self.params = {SPOT_DIA_STR: 5,
+                       NOISE_DIA_STR: 1,
+                       SPOT_BRIGHTNESS_STR: 50}
+
+    def roleNames(self):
+        """See base class."""
+        role_names = super().roleNames()
+        role_names[PROPERTY_NAME_ROLE] = b'propertyName'
+        role_names[Qt.UserRole + 10] = b'min'
+        role_names[Qt.UserRole + 11] = b'max'
+        role_names[Qt.UserRole + 12] = b'step'
+        return role_names
+
+    def rowCount(self, parent: QtCore.QModelIndex = None) -> int:
+        """See base class."""
+        return len(self.params)
+
+    def data(self, index: QtCore.QModelIndex, role: int = Qt.DisplayRole):
+        """See base class."""
+        name = self.property_names[index.row()]
+        if role == Qt.DisplayRole:
+            return self.params[name]
+        if role == Qt.EditRole:
+            return self.params[name]
+        if role == PROPERTY_NAME_ROLE:
+            return name
+        if role == Qt.UserRole + 10:
+            return SPOT_PARAMS_RANGE[name].min
+        if role == Qt.UserRole + 11:
+            return SPOT_PARAMS_RANGE[name].max
+        if role == Qt.UserRole + 12:
+            return SPOT_PARAMS_RANGE[name].step
+
+    def setData(self, index: QtCore.QModelIndex, value: typing.Any,
+                role: int = None) -> bool:
+        """See base class."""
+        if index.isValid() and role == Qt.EditRole:
+            name = self.property_names[index.row()]
+            self.params[name] = value
+            return True
+        return False
+
+    def flags(self, index: QtCore.QModelIndex) -> Qt.ItemFlags:
+        """See base class."""
+        if not index.isValid():
+            return Qt.NoItemFlags
+        return Qt.ItemIsEditable | Qt.ItemIsEnabled
+
 
 class AoisView(QtCore.QObject):
     pick_aois = QtCore.Signal()
@@ -101,15 +181,19 @@ class Window(QtWidgets.QWidget):
         self.qml = QtQuickWidgets.QQuickWidget()
         # self.qml.setResizeMode(QtQuickWidgets.QQuickWidget.SizeRootObjectToView)
         qml_path = Path(__file__).parent / 'image_view.qml'
-        self.qml.setSource(QtCore.QUrl(str(qml_path)))
         root_context = self.qml.rootContext()
         root_context.setContextProperty('imageView', self.image_view)
+        root_context.setContextProperty('dataModel', self.image_view.model)
+        # root_context.setContextProperty('hhh', self.image_view.model.pickSpotsParam)
+        # Need to set context property before set source
+        self.qml.setSource(QtCore.QUrl(str(qml_path)))
 
         layout.addWidget(self.image_view)
         layout.addWidget(self.qml, 0, 1)
         layout.setColumnStretch(0, 4)
         layout.setColumnStretch(1, 1)
         self.setLayout(layout)
+
 
 def main():
     path = Path('/run/media/tzu-yu/linuxData/Git_repos/python_for_imscroll/python_for_imscroll/test/test_data/fake_im/')
@@ -132,7 +216,11 @@ def main():
 
 
     ## Start the Qt event loop
-    sys.exit(app.exec_())
+    app.exec_()
+    model = window.image_view.model # This line here is to make sure the context property lives before qml quits
+    # To avoid a warning thrown by the qml
+    # cannot think of a better way right now
+    sys.exit()
 
 
 if __name__ == '__main__':
--- gui/image_view.qml
+++ gui/image_view.qml
@@ -3,7 +3,7 @@ import QtQuick.Controls 2.15
 
 Item{
     id: root
-    width: 200; height: 480
+    width: 300; height: 480
 
     Button {
         id: button1
@@ -11,4 +11,85 @@ Item{
         onClicked: imageView.onPickButtonPressed()
 
     }
+
+    ListView {
+        anchors {
+            left: parent.left
+            right: parent.right
+            top: button1.bottom
+            bottom: parent.bottom
+        }
+        model: dataModel.pickSpotsParam
+        interactive: false
+        spacing: 20
+        delegate: Item {
+            id: entryRoot
+            implicitHeight: 30
+            anchors {
+                left: parent.left; right: parent.right
+            }
+            Item {
+                id: entryTextRegion
+                anchors {
+                    fill: parent
+                    leftMargin: 5
+                    rightMargin: parent.width - 120
+                }
+                Text {
+                    anchors.fill: parent
+                    text: model.propertyName
+                    verticalAlignment: Text.AlignVCenter
+                }
+            }
+            SpinBox {
+                id: control
+                implicitWidth: 80
+                down.indicator.implicitHeight: 20
+                down.indicator.implicitWidth: 20
+                up.indicator.implicitHeight: 20
+                up.indicator.implicitWidth: 20
+                from: model.min
+                to: model.max
+                anchors {
+                    left: parent.left
+                    leftMargin: 120
+                }
+                contentItem: TextInput {
+                    // This item was copied from the source code, I only needed
+                    // to modify its selectByMouse property
+                    z: 2
+                    text: control.displayText
+
+                    font: control.font
+                    color: control.palette.text
+                    selectionColor: control.palette.highlight
+                    selectedTextColor: control.palette.highlightedText
+                    horizontalAlignment: Qt.AlignHCenter
+                    verticalAlignment: Qt.AlignVCenter
+
+                    readOnly: !control.editable
+                    validator: control.validator
+                    inputMethodHints: control.inputMethodHints
+                    selectByMouse: true
+
+                    Rectangle {
+                        x: -6 - (control.down.indicator ? 1 : 0)
+                        y: -6
+                        width: control.width - (control.up.indicator ? control.up.indicator.width - 1 : 0) - (control.down.indicator ? control.down.indicator.width - 1 : 0)
+                        height: control.height
+                        visible: control.activeFocus
+                        color: "transparent"
+                        border.color: control.palette.highlight
+                        border.width: 2
+                    }
+                }
+
+                value: model.display
+                editable: true
+                onValueModified: {model.edit = value}
+
+            }
+        }
+    }
+
 }
