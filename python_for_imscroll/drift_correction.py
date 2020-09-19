f5375f309d0aca50e314dd76e5c4f7d7c414ea45
Tzu-Yu Lee <leejuyuu@gmail.com>
2020-09-19 19:46:53 +0800

    Add DriftCorrector and AOI shifting function
    This class in the drift_correction module will move the AOIs
    according to the driftlist.

--- python_for_imscroll/drift_correction.py
+++ python_for_imscroll/drift_correction.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 import scipy.signal
+import python_for_imscroll.image_processing as imp
 
 
 def make_drift_list_simple(drift_fit):
@@ -24,3 +25,20 @@ def make_drift_list_simple(drift_fit):
     driftlist[:, 0] = np.arange(end_frame)
     driftlist[start_frame:end_frame+1, 1:] = np.diff(filtered_displacement, axis=0)
     return driftlist
+
+
+class DriftCorrector:
+    def __init__(self, driftlist):
+        self._driftlist = driftlist
+
+    def shift_aois(self, aois, frame):
+        start_frame = aois.frame
+        drift_range = np.logical_and(self._driftlist[:, 0] >= start_frame + 1,
+                                     self._driftlist[:, 0] <= frame)
+        drift = self._driftlist[drift_range, 1:].sum(axis=0)[np.newaxis, :]
+        new_aois = imp.Aois(aois._coords + drift,
+                            frame=frame,
+                            width=aois.width,
+                            frame_avg=aois.frame_avg,
+                            channel=aois.channel)
+        return new_aois
--- python_for_imscroll/test/test_drift_correction.py
+++ python_for_imscroll/test/test_drift_correction.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import numpy as np
 import scipy.io as sio
 import python_for_imscroll.drift_correction as dcorr
+import python_for_imscroll.image_processing as imp
 
 TEST_DATA_DIR = Path(__file__).parent / 'test_data'
 
@@ -13,3 +14,40 @@ def test_make_driftlist_simple():
     driftlist[:, 0] += 1
     correct_driftlist = sio.loadmat(TEST_DATA_DIR / '20200228/L2_driftlist.dat')['driftlist']
     np.testing.assert_allclose(driftlist, correct_driftlist[:, :3], rtol=0, atol=1e-11)
+
+def test_drift_corrector():
+    driftlist = np.ones((10, 3))
+    driftlist[:, 0] = np.arange(10)
+    driftlist[:, 2] = 2
+    driftlist[0, :] = 0
+    drifter = dcorr.DriftCorrector(driftlist)
+    np.testing.assert_equal(drifter._driftlist, driftlist)
+    arr = np.random.uniform(0, 100, (100, 2))
+
+    # Starting from the first frame
+    aois = imp.Aois(arr, frame=0, frame_avg=1, width=7, channel='blue')
+    for frame in range(10):
+        new_aois = drifter.shift_aois(aois, frame)
+        assert isinstance(new_aois, imp.Aois)
+        assert new_aois.frame == frame
+        assert new_aois.frame_avg == aois.frame_avg
+        assert new_aois.width == aois.width
+        assert new_aois.channel == aois.channel
+        correct_arr = arr[:, :]
+        correct_arr[:, 0] += frame
+        correct_arr[:, 1] += frame*2
+        np.testing.assert_allclose(new_aois._coords, correct_arr)
+
+    # Starting from other frames
+    aois = imp.Aois(arr, frame=2, frame_avg=1, width=7, channel='blue')
+    for frame in range(2, 10):
+        new_aois = drifter.shift_aois(aois, frame)
+        assert isinstance(new_aois, imp.Aois)
+        assert new_aois.frame == frame
+        assert new_aois.frame_avg == aois.frame_avg
+        assert new_aois.width == aois.width
+        assert new_aois.channel == aois.channel
+        correct_arr = arr[:, :]
+        correct_arr[:, 0] += frame-2
+        correct_arr[:, 1] += (frame-2)*2
+        np.testing.assert_allclose(new_aois._coords, correct_arr)
