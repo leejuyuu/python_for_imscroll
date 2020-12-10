39d6d3229cc64a127b190f5a4323815ecb1534c3
Tzu-Yu Lee <leejuyuu@gmail.com>
2020-12-10 11:14:53 +0800

    Update combine survival curves

--- python_for_imscroll/script_combine_survival_plots.py
+++ python_for_imscroll/script_combine_survival_plots.py
@@ -16,35 +16,37 @@ FILL_COLORS = ['#e2e1b2',
 '']
 
 def main():
-    datapath = Path('/run/media/tzu-yu/data/PriA_project/Analysis_Results/20200228/20200228imscroll/')
-    filestr = ['L1', 'L2', 'L3', 'L5']
-    num = [500, 125, 62.5, 250]
+    datapath = Path('/run/media/tzu-yu/data/PriA_project/Analysis_Results/20200825/20200825imscroll/')
+    filestr = ['L1', 'L2', 'L3', 'L4']
+    num = [62.5, 125, 250, 500]
     sorting = np.argsort(num)
-    labels = ['500 pM', '125 pM', '62.5 pM', '250 pM']
+    labels = ['62.5 pM', '125 pM', '250 pM', '500 pM']
     filestr = [filestr[i] for i in sorting]
     labels = [labels[i] for i in sorting]
     fig, ax = plt.subplots(figsize=(4, 3))
     sns.despine(fig, ax)
 
     for file, label, color, fill_color  in zip(filestr, labels, sns.color_palette(palette='muted'), sns.color_palette(palette='pastel')):
-        filepath = datapath / '{}__first_dwellr.hdf5'.format(file)
-        with h5py.File(filepath, 'r') as f:
-            surv_data = f.get('/survival_curve/data')[()]
-            param = f.get('/bi_exp_model/param')[()]
+        filepath = datapath / '{}_first_on.npz'.format(file)
+        npz_file = np.load(filepath)
 
-        time = surv_data[0]
-        surv = surv_data[1]
+        time = npz_file['survival_curve']['time']
+        surv = npz_file['survival_curve']['surv']
+        upper_ci = npz_file['survival_curve']['upper_ci']
+        lower_ci = npz_file['survival_curve']['lower_ci']
         S = lambda t, k1, k2, A: A*np.exp(-k1*t) + (1-A)*np.exp(-k2*t)
+        param = npz_file['param']
         x = np.linspace(0, time[-1], int(round(time[-1]*10)))
         y = S(x, *param)
 
         ax.step(time, surv, where='post', color=color, label=label)
         ax.plot(x, y, color=color)
         fill_color = fill_color + (80/255,)
-        ax.fill_between(time, surv_data[2], surv_data[3], step='post', color=fill_color)
+        # ax.fill_between(time, lower_ci, upper_ci, step='post', color=fill_color)
     ax.set_ylim((0, 1.05))
     ax.set_xlim((0, time[-1]))
-    ax.set_xlabel('Time (s)', fontsize=14)
+    ax.set_xlim((0, 500))
+    ax.set_xlabel(r'$t_{on}$ (s)', fontsize=14)
     ax.set_ylabel('Survival probability', fontsize=14)
     ax.legend(frameon=False)
     plt.rcParams['svg.fonttype'] = 'none'
