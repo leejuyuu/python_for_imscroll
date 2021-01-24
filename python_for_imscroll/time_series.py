e195f9a67faae1823cdbbd9984e757d6ff0e649c
Tzu-Yu Lee <leejuyuu@gmail.com>
2021-01-24 15:20:39 +0800

    Modify TimeTraces to pass the last test
    - Populate __init__() method
    - Add a method to set variables such as intensities during image
      processing
    
    Todo: The classmethod that loads from the '_all.json' file is broken
    now, need to fix it later.

--- python_for_imscroll/time_series.py
+++ python_for_imscroll/time_series.py
@@ -1,36 +1,55 @@
 from pathlib import Path
+import numpy as np
+import xarray as xr
 from python_for_imscroll import binding_kinetics as bk
 
 
 class TimeTraces():
 
-    def __init__(self):
-        self.data = None
+    def __init__(self, n_traces, channels=None):
+        if channels is None:
+            self._data = None
+        else:
+            self._data = dict.fromkeys(channels.keys())
+        self.n_traces = n_traces
+        self._time = channels
         self.aoi_categories = None
 
     def get_n_traces(self):
-        return len(self.data.AOI)
+        return len(self._data.AOI)
 
     def get_channels(self):
-        channels = [self.data.target_channel] + self.data.binder_channel
-        return channels
+        return list(self._data.keys())
 
     @classmethod
     def from_xarray_json_all_file(cls, path):
         all_data, aoi_categories = bk.load_all_data(path)
-        time_traces = cls()
-        time_traces.data = all_data['data']
+        time_traces = cls(0)
+        time_traces._data = all_data['data']
         time_traces.aoi_categories = aoi_categories
         return time_traces
 
     def get_time(self, channel, molecule):
-        data = self.data.sel(channel=channel, AOI=molecule)
+        data = self._data.sel(channel=channel, AOI=molecule)
         return data.time.values
 
     def get_intensity(self, channel, molecule):
-        data = self.data.sel(channel=channel, AOI=molecule)
+        data = self._data[channel].sel(molecule=molecule)
         return data.intensity.values
 
+    def set_value(self, variable_name, channel, time, array):
+        if len(array) != self.n_traces:
+            raise ValueError(f'Input array length ({len(array)}) does '
+                             f'not match n_traces ({self.n_traces}).')
+        if self._data[channel] is None:
+            time_arr = self._time[channel]
+            data = {variable_name: (['molecule', 'time'], np.zeros((self.n_traces, len(time_arr))))}
+            self._data[channel] = xr.Dataset(data,
+                                             coords={'molecule': (['molecule'], np.arange(self.n_traces)),
+                                                     'time': (['time'], time_arr)})
+        self._data[channel][variable_name].loc[dict(time=time)] = array
+
+
     def get_state_mean_sequence(self, channel, molecule):
-        data = self.data.sel(channel=channel, AOI=molecule)
+        data = self._data.sel(channel=channel, AOI=molecule)
         return data.viterbi_path.sel(state='position').values
